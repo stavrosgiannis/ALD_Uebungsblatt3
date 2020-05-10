b98a88f753a2afc4c931eb410f23855c2cd56ab5
Stavros Gia <stavros.g@web.de>
2020-05-10 23:21:51 +0200

    fix

--- ALD_Uebungsblatt3/ALD_Uebungsblatt3.cpp
+++ ALD_Uebungsblatt3/ALD_Uebungsblatt3.cpp
@@ -35,6 +35,22 @@ int main()
 	}
 
 	// Testtreiber
+	/*
+	Runtime tests for random order insertion:
+	-> Runtime for 2^12 values: 0.005
+	-> Runtime for 2^13 values: 0.01
+	-> Runtime for 2^14 values: 0.022
+	-> Runtime for 2^15 values: 0.05
+	-> Runtime for 2^16 values: 0.116
+
+	Runtime tests for ascending order insertion:
+	-> Runtime for 2^12 values: 0.364
+	-> Runtime for 2^13 values: 0.735
+	-> Runtime for 2^14 values: 1.466
+	-> Runtime for 2^15 values: 2.937
+	-> Runtime for 2^16 values: 5.943
+	-> Runtime for 2^17 values: 11.906
+	*/
 	messung1();
 	messung2();
 
